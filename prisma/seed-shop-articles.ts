b969c907a21d726ed7dc18522d2492ea88815e43
helixlit <helixlit@proton.me>
2026-08-17 12:36:51 +0200

    fix: made error more verbose

--- prisma/seed-shop-articles.ts
+++ prisma/seed-shop-articles.ts
@@ -6,35 +6,37 @@ import { promises as fs } from "fs";
 import "dotenv/config";
 
 const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL,
 });
 
 const prisma = new PrismaClient({
-    adapter,
+  adapter,
 });
 
 export async function main() {
-    const articlePzns: Array<string> = await JSON.parse(
-        await fs.readFile("prisma/shop-articles.json", "utf8")
-    );
+  const articlePzns: Array<string> = await JSON.parse(
+    await fs.readFile("prisma/shop-articles.json", "utf8"),
+  );
 
-    for (let articlePzn of articlePzns) {
-        try {
-            const article = await prisma.article.findUniqueOrThrow({
-                where: {
-                    pzn: articlePzn
-                }
-            });
+  for (const articlePzn of articlePzns) {
+    try {
+      const article = await prisma.article.findUniqueOrThrow({
+        where: {
+          pzn: articlePzn,
+        },
+      });
 
-            await prisma.shopArticle.create({
-                data: { articlePZN: article.pzn }
-            })
+      await prisma.shopArticle.create({
+        data: { articlePZN: article.pzn },
+      });
 
-            console.debug(`Created ShopArticle for PZN ${articlePzn}`)
-        } catch (e) {
-            console.error(`Could not add Article to ShopArticles, because PZN ${articlePzn} was not found :(`);
-        }
+      console.debug(`Created ShopArticle for PZN ${articlePzn}`);
+    } catch (e) {
+      console.error(
+        `Could not add Article with PZN ${articlePzn} to ShopArticles! Error: ${e}`,
+      );
     }
+  }
 }
 
 main();
